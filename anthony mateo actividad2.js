79b4220362b3ed31ec4e7fea3c1af710fa175ae6
¨Anthony <26005467@galileo.edu>
2026-02-06 22:40:40 -0600

    first commit

---
+++ anthony mateo actividad2.js
@@ -0,0 +1,46 @@
+const mes = parseInt(prompt(" Usuario Ingrese su mes de nacimiento 1-12"));
+  const dia = parseInt(prompt("Usuario Ingrese dia día de nacimiento"));
+
+var signo = "";
+
+if ((mes === 3 && dia >= 21) || (mes === 4 && dia <= 20)) 
+{signo = "Aries";}
+
+else if ((mes === 4 && dia >= 21) || (mes === 5 && dia <= 20)) 
+{signo = "Tauro";}
+
+else if ((mes === 5 && dia >= 21) || (mes === 6 && dia <= 20)) 
+{signo = "Géminis";}
+
+else if ((mes === 6 && dia >= 21) || (mes === 7 && dia <= 22)) 
+{signo = "Cáncer";}
+
+else if ((mes === 7 && dia >= 23) || (mes === 8 && dia <= 22)) 
+{signo = "Leo";}
+
+else if ((mes === 8 && dia >= 24) || (mes === 9 && dia <= 22)) 
+{signo = "Virgo";}
+
+else if ((mes === 9 && dia >= 23) || (mes === 10 && dia <= 23)) 
+{signo = "Libra";}
+
+else if ((mes === 10 && dia >= 24) || (mes === 11 && dia <= 22)) 
+{signo = "Escorpio";}
+
+else if ((mes === 11 && dia >= 23) || (mes === 12 && dia <= 21)) 
+{signo = "Sagitario";}
+
+else if ((mes === 12 && dia >= 22) || (mes === 1 && dia <= 20)) 
+{signo = "Capricornio";}
+
+else if ((mes === 1 && dia >= 21) || (mes === 2 && dia <= 19)) 
+{signo = "Acuario";}
+
+else if ((mes === 2 && dia >= 20) || (mes === 3 && dia <= 20)) 
+{signo = "Piscis";} 
+
+else  {
+    signo = "Fecha no válida";
+}
+
+alert("Tu signo zodiacal es: " + signo);
